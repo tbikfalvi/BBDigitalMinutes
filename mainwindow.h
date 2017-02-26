f1d2cffb9271770d2dfff530a2155d451695a620
Tamas Bikfalvi <tbikfalvi@gmail.com>
2017-02-26 10:53:38 +0100

    More images base functionalities on GUI

--- mainwindow.h
+++ mainwindow.h
@@ -2,10 +2,33 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFrame>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QSystemTrayIcon>
 
-namespace Ui {
-class MainWindow;
-}
+class cPanelPlayer : public QFrame
+{
+    Q_OBJECT
+
+public:
+    QHBoxLayout     *hlPlayer;
+    QFrame          *frmPlayerNumber;
+    QLabel          *lblPlayerNumber;
+    QFrame          *frmPlayerName;
+    QLabel          *lblPlayerName;
+    QFrame          *frmFault;
+    QLabel          *lblPlayerFault1;
+    QLabel          *lblPlayerFault2;
+    QLabel          *lblPlayerFault3;
+    QLabel          *lblPlayerFault4;
+    QLabel          *lblPlayerFault5;
+
+    cPanelPlayer( QWidget *p_poParent = 0, QString p_qsPlayerNumber = "", QString p_qsPlayerName = "" );
+
+};
+
+namespace Ui { class MainWindow; }
 
 class MainWindow : public QMainWindow
 {
@@ -15,8 +38,46 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+protected:
+    void timerEvent( QTimerEvent *p_poEvent );
+
+private slots:
+    void on_pbContinueMainTimer_clicked();
+    void on_pbSignalReferee_clicked();
+    void on_pbEditMainTime_clicked();
+    void on_pbSaveMainTime_clicked();
+    void on_pbCancelSaveMainTime_clicked();
+    void on_pbRequestTimeHome_clicked();
+    void on_pbRequestTimeGuest_clicked();
+    void on_pbHomePlay_clicked();
+    void on_pbGuestPlay_clicked();
+
+    void on_pnIncreaseQuarter_clicked();
+
+    void on_pnDecreaseQuarter_clicked();
+
 private:
-    Ui::MainWindow *ui;
+
+    Ui::MainWindow          *ui;
+    QSystemTrayIcon         *m_stIcon;
+
+    int                      nTimerMainPlayTime;
+    int                      nTimeMainMiliSec;
+    int                      nTimerTimeDead;
+    int                      nTimeDeadSecond;
+    bool                     bTeamHomePlay;
+    bool                     bTeamGuestPlay;
+    int                      nTimerTeamPlayTime;
+    int                      nTimeTeamPlaySecond;
+
+    void                    _updateMainPlayTime();
+    void                    _updateTeamHomePlayTime();
+    void                    _updateTeamGuestPlayTime();
+    void                    _updateDeadTime();
+    void                    _resetDeadTime();
+    void                    _showTrayInfo( QString p_qsMessage );
+    void                    _showTrayWarning( QString p_qsMessage );
+    void                    _showTrayError( QString p_qsMessage );
 };
 
 #endif // MAINWINDOW_H
